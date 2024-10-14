ff5ee7e02c32053c702f8855813ebdf1d3af7925
Fredeau123 <frederictrivett@gmail.com>
2024-10-14 14:50:00 +0200

    update

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -8,7 +8,7 @@ interface EndScreenProps {
 const EndScreen: React.FC<EndScreenProps> = ({ formData }) => {
   return (
     <div className="text-center">
-      <h2 className="title-gradient text-5xl font-bold mb-8">Thank you, {formData.name}!</h2>
+      <h2 className="title-gradient text-6xl leading-[0.9] mb-2">Thank you, {formData.name}!</h2>
       <p className="text-xl mb-4">You&apos;ll be among the first to know when Growvy launches.</p>
       <p className="text-xl">Stay tuned for exciting updates!</p>
     </div>
