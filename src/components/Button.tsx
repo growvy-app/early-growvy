2426e7eabcdbf08820846ba1b2a29b93354b99b1
Fredeau123 <frederictrivett@gmail.com>
2024-10-14 14:38:02 +0200

    update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
   children,
   className = '',
 }) => {
-  const baseStyle = "relative py-3.5 px-10 font-medium text-lg rounded-lg transition-all duration-200 overflow-hidden";
+  const baseStyle = "relative py-3.5 px-10 font-medium text-lg rounded-xl transition-all duration-200 overflow-hidden";
   const primaryStyle = "text-white hover:opacity-90 bg-primary-gradient";
   const disabledStyle = "bg-gray-300 text-gray-500 cursor-not-allowed";
 
@@ -28,9 +28,10 @@ const Button: React.FC<ButtonProps> = ({
       disabled={disabled}
       className={`${baseStyle} ${buttonStyle} ${className}`}
     >
-      {!disabled && (
-        <span className="absolute inset-[2px] inset-b-[1px] rounded-[6px] bg-primary-gradient" style={{ filter: 'brightness(1.2)' }}></span>
-      )}
+      <span 
+        className={`absolute inset-[2px] inset-b-[0px] rounded-[10px] ${disabled ? 'bg-gray-200' : 'bg-primary-gradient'}`} 
+        style={{ filter: disabled ? 'brightness(1.05)' : 'brightness(1.2)' }}
+      ></span>
       <span className="relative z-10">{children}</span>
     </button>
   );
