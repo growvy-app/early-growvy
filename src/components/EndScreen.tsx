6b3bcdb19006f6268c4f842dd5669da87d6510ec
Fredeau123 <frederictrivett@gmail.com>
2024-10-15 22:16:35 +0200

    perfect now deploy!

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -10,19 +10,23 @@ interface EndScreenProps {
 const EndScreen: React.FC<EndScreenProps> = ({ formData }) => {
   const [windowDimension, setWindowDimension] = useState({ width: 0, height: 0 });
   const [confettiOpacity, setConfettiOpacity] = useState(1);
+  const [isConfettiActive, setIsConfettiActive] = useState(true);
 
   useEffect(() => {
     setWindowDimension({ width: window.innerWidth, height: window.innerHeight });
 
     const fadeOutConfetti = () => {
       setConfettiOpacity((prevOpacity) => {
-        if (prevOpacity <= 0) return 0;
+        if (prevOpacity <= 0.1) {
+          setIsConfettiActive(false);
+          return 0;
+        }
         return prevOpacity - 0.02;
       });
     };
 
-    const confettiInterval = setInterval(fadeOutConfetti, 50);
-    const confettiTimeout = setTimeout(() => clearInterval(confettiInterval), 3000);
+    const confettiInterval = setInterval(fadeOutConfetti, 100);
+    const confettiTimeout = setTimeout(() => clearInterval(confettiInterval), 5000);
 
     return () => {
       clearInterval(confettiInterval);
@@ -46,14 +50,16 @@ const EndScreen: React.FC<EndScreenProps> = ({ formData }) => {
 
   return (
     <div className="text-center">
-      <Confetti
-        width={windowDimension.width}
-        height={windowDimension.height}
-        opacity={confettiOpacity}
-        recycle={false}
-        numberOfPieces={200}
-        colors={['#786AFE', '#9370DB', '#8A2BE2', '#9932CC', '#BA55D3']} // Various purple shades
-      />
+      {isConfettiActive && (
+        <Confetti
+          width={windowDimension.width}
+          height={windowDimension.height}
+          opacity={confettiOpacity}
+          recycle={false}
+          numberOfPieces={200}
+          colors={['#786AFE', '#9370DB', '#8A2BE2', '#9932CC', '#BA55D3']} // Various purple shades
+        />
+      )}
       <h2 className="title-gradient text-6xl leading-[0.9] mb-2">Thank you, {formData.name}!</h2>
       <p className="text-xl mb-4">You&apos;ll be among the first to know when Growvy launches.</p>
       <p className="text-xl mb-8">Stay tuned for exciting updates!</p>
